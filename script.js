db509a0dc1bc5a399f0c823b61426ab797f8e57f
wafib <wafibed@gmail.com>
2023-10-09 21:17:46 +0300

    calc
    updated to remove that part

--- script.js
+++ script.js
@@ -1,54 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const calculateMilesBtn = document.getElementById('calculateMilesBtn');
-    const originInput = document.getElementById('origin');
-    const destinationInput = document.getElementById('destination');
-    const resultLabel = document.getElementById('resultLabel');
+document.addEventListener("DOMContentLoaded", function () {
+    const calculateButton = document.getElementById("calculate");
 
-    calculateMilesBtn.addEventListener('click', function () {
-        const origin = originInput.value.toUpperCase();
-        const destination = destinationInput.value.toUpperCase();
+    calculateButton.addEventListener("click", function () {
+        const fareClass = document.getElementById("fare-class").value;
+        const milesFlown = parseFloat(document.getElementById("miles-flown").value);
+        const membershipStatus = document.getElementById("membership-status").value;
+        const bonusPercentage = parseFloat(document.getElementById("bonus-percentage").value);
 
-        try {
-            // Use the geodesic function to calculate distance
-            const originCoordinates = { latitude: airports[origin].lat, longitude: airports[origin].lon };
-            const destinationCoordinates = { latitude: airports[destination].lat, longitude: airports[destination].lon };
-            const distance = geodesic(originCoordinates, destinationCoordinates).miles;
+        const fareEarningRates = {
+            "First Class Flex": 3.5,
+            "Business Class Flex": 2.5,
+            // Add other fare class rates here
+        };
 
-            // Update the result label
-            resultLabel.textContent = `Distance between ${origin} and ${destination}: ${distance.toFixed(2)} miles`;
-        } catch (error) {
-            // Handle errors
-            resultLabel.textContent = 'Error: Unable to get coordinates for one or both airports';
+        const bonusMultipliers = {
+            "Green": 1.0,
+            "Silver": 1.25,
+            "Gold": 1.5,
+        };
+
+        const isDomestic = true; // You can implement this logic as needed
+
+        const bonusMultiplier = bonusMultipliers[membershipStatus];
+        const earningRate = fareEarningRates[fareClass];
+
+        if (isNaN(milesFlown) || isNaN(bonusPercentage)) {
+            displayResult("Please enter valid numbers.");
+        } else {
+            const totalMileage = calculateMileage(milesFlown, earningRate, bonusMultiplier);
+            displayResult(`Total mileage with bonus: ${totalMileage.toFixed(2)} miles`);
         }
     });
 
-    const calculateMileageBtn = document.getElementById('calculateMileageBtn');
-    const fareClassSelect = document.getElementById('fareClass');
-    const milesFlownInput = document.getElementById('milesFlown');
-    const membershipStatusSelect = document.getElementById('membershipStatus');
-    const domesticFlightCheckbox = document.getElementById('domesticFlight');
-    const bonusPercentageInput = document.getElementById('bonusPercentage');
-    const totalMileageLabel = document.getElementById('totalMileageLabel');
+    function calculateMileage(milesFlown, earningRate, bonusMultiplier) {
+        return milesFlown * earningRate * bonusMultiplier;
+    }
 
-    calculateMileageBtn.addEventListener('click', function () {
-        const fareClass = fareClassSelect.value;
-        const milesFlown = parseFloat(milesFlownInput.value);
-        const membershipStatus = membershipStatusSelect.value;
-        const isDomestic = domesticFlightCheckbox.checked;
-        const bonusPercentage = parseFloat(bonusPercentageInput.value);
-
-        // Define fare class earning rates
-        const earningRates = isDomestic ? fareEarningRates.Domestic : fareEarningRates.International;
-
-        // Get the earning rate for the selected fare class
-        const earningRate = earningRates[fareClass] || 0;
-
-        // Calculate total mileage
-        const baseMileage = milesFlown * earningRate;
-        const bonusMultiplier = bonusMultipliers[membershipStatus] || 1.0;
-        const totalMileage = baseMileage * bonusMultiplier;
-
-        // Update the total mileage label
-        totalMileageLabel.textContent = `Total mileage with bonus: ${totalMileage.toFixed(2)} miles`;
-    });
+    function displayResult(resultText) {
+        const resultDiv = document.getElementById("result");
+        resultDiv.textContent = resultText;
+    }
 });
